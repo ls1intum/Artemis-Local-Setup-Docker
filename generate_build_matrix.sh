a023909e752a624944ff82ec9795fb00f56b3864
Henning Hontheim <henning@hontheim.net>
2022-04-21 19:57:53 +0200

    Add the magic matrix script.
    This bash script generates the build matrix for only the changed images. It will only append those directories that contain a changed (or added) `Dockerfile` or `RELEASE`-file. It will then extract the version from the `RELEASE`-file and adds it to the matrix – this information will be used by the buildx-step which injects it into the Dockerfile.
    
    Finally, it generates two flags `build_base_images` and `build_bamboo_variants` that will also be used by the workflow to skip steps completely.

---
+++ generate_build_matrix.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+CHANGED_DIRS=$(git diff-tree --no-commit-id --name-only -r ${GITHUB_SHA} -m '*Dockerfile' '*RELEASE' | xargs -I {} dirname {} | uniq)  # Only matches, if `Dockerfile` or `RELEASE` is changed!
+echo -n "Changes in directories: "
+echo $CHANGED_DIRS
+
+MATRIX_BASE_INCLUDE=""
+MATRIX_VARIANTS_INCLUDE=""
+
+for DIR in ${CHANGED_DIRS}; do
+    if [[ -f "${DIR}/RELEASE" ]]; then
+        DO_BUILD=$(grep "BUILD=" "${DIR}"/RELEASE | awk -F '=' '{ print $2}')
+        if [[ "${DO_BUILD}" != "yes" ]]; then
+            echo "Skipping ${DIR} because BUILD is not set to yes."
+        elif [[ "${DIR}" != *"bamboo/c"* && "${DIR}" != *"bamboo/swift"* ]]; then # Skip C & Swift. They will only be re-built, if the Dockerfile for bamboo is changed.
+            NAME=$DIR
+            VERSION=$(grep "VERSION=" "${NAME}"/RELEASE | awk -F '=' '{ print $2}')
+            MATRIX_BASE_INCLUDE+="{\"name\": \"${NAME}\", \"version\": \"${VERSION}\"}"
+        elif [[ "${DIR}" = *"bamboo/"* ]]; then
+            NAME=$(echo ${DIR} | awk -F '/' '{ print $2}')
+            VERSION=$(grep "VERSION=" "${DIR}"/RELEASE | awk -F '=' '{ print $2}')
+            PLATFORMS=$(grep "PLATFORMS=" "${DIR}"/RELEASE | awk -F '=' '{ print $2}')
+            MATRIX_VARIANTS_INCLUDE+="{\"name\": \"${NAME}\", \"version\": \"${VERSION}\", \"platforms\": \"${PLATFORMS}\"}"
+        fi
+    else
+        echo "Skipping directory \"${DIR}\" because it does not contain a RELEASE file."
+    fi
+done
+
+SPLIT='}, {'
+MATRIX_BASE_INCLUDE="[${MATRIX_BASE_INCLUDE//\}\{/$SPLIT}]"
+MATRIX_BASE="{\"include\": ${MATRIX_BASE_INCLUDE}}"
+
+MATRIX_VARIANTS_INCLUDE="[${MATRIX_VARIANTS_INCLUDE//\}\{/$SPLIT}]"
+MATRIX_VARIANTS="{\"include\": ${MATRIX_VARIANTS_INCLUDE}}"
+
+echo "Matrix for base images: ${MATRIX_BASE}"
+echo "Matrix in directories for variants images: ${MATRIX_VARIANTS}"
+
+BUILD_BASE_IMAGES="no"
+if [[ "${MATRIX_BASE_INCLUDE}" != "[]" ]]; then
+    BUILD_BASE_IMAGES="yes"
+fi
+
+BUILD_BAMBOO_VARIANTS="no"
+if [[ "${MATRIX_VARIANTS_INCLUDE}" != "[]" ]]; then
+    BUILD_BAMBOO_VARIANTS="yes"
+fi
+
+echo "::set-output name=matrix::${MATRIX_BASE}"
+echo "::set-output name=matrix_variants::${MATRIX_VARIANTS}"
+echo "::set-output name=build_base_images::${BUILD_BASE_IMAGES}"
+echo "::set-output name=build_bamboo_variants::${BUILD_BAMBOO_VARIANTS}"
